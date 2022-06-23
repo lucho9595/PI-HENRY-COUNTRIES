f2727f0480ea31711024761f0f61e9c8bf5b3043
lucho9595 <lucianocoronel48@gmail.com>
2022-06-23 16:49:57 -0300

    Client Pi Countries

--- client/Actions/index.js
+++ client/Actions/index.js
@@ -1,10 +1,12 @@
-const axios = require ('axios');
+const axios = require('axios');
 export const GET_COUNTRIES = "GET_COUNTRIES";
 export const GET_ID_COUNTRIES = "GET_ID_COUNTRIES";
+export const GET_NAME = " GET_NAME";
 
+//TRAER TODOS LOS PAISES
 
-export function getAllCountries(){
-    return async function (dispatch){
+export function getAllCountries() {
+    return async function (dispatch) {
         var json = await axios.get('http://localhost:3001/countries')
         return dispatch({
             type: GET_COUNTRIES,
@@ -13,8 +15,10 @@ export function getAllCountries(){
     }
 }
 
-export function getIdCountries (id){
-    return async function (dispatch){
+//TRAER PAISES POR ID
+
+export function getIdCountries(id) {
+    return async function (dispatch) {
         var json = await axios.get(`http://localhost:3001/countries/${id}`)
         return dispatch({
             type: GET_ID_COUNTRIES,
@@ -22,3 +26,18 @@ export function getIdCountries (id){
         })
     }
 }
+
+//TRAER PAISES POR NOMBRE
+
+export function getName(name) {
+    return async function (dispatch) {
+        var json = await axios.get(`http://localhost:3001/countries?name=${name}`)
+        return dispatch({
+            type: GET_NAME,
+            payload: json.data
+        })
+    }
+}
+
+
+
--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      <h1>Puto el que lee</h1>
+      
     </div>
   );
 }
